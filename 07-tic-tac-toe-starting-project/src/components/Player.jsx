9b08046efc290cc532d18e38a198985cdada6f48
Vikas biradar <vikasbiradar2016@gmail.com>
2025-04-04 11:49:12 +0530

    Added game logic

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Player({initialName,symbol}){
+export default function Player({initialName,symbol, isActive}){
     const [playerName, setPlayerName] = new useState(initialName);
     const [isEditing, setIsEditing] = new useState(false);
     
@@ -17,7 +17,7 @@ export default function Player({initialName,symbol}){
         editablePlayerName = <input type="text" value={playerName} onChange={handleChange} required/>
     }
     return(
-        <li>
+        <li className={isActive ? 'active' : ''}>
             <span className="player">
               {editablePlayerName}
               <span className="player-symbol">{symbol}</span>
